eb406ac653187c6f961b446937e3908275972a88
Marigiko <mark221@hotmail.es>
2022-02-11 13:40:07 -0300

    Changes

--- src/main/java/com/cinekodigo/cinekodigo/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cinekodigo.cinekodigo.repository;
-
-import com.cinekodigo.cinekodigo.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-}
